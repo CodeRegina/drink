07898dbec8e697542d5e0bb019ec41d25b66dab5
Regina Ward <regina.ward94@outlook.com>
2023-02-16 04:35:45 -0600

    used program.cs class to migrate and seed the data

--- API/Program.cs
+++ API/Program.cs
@@ -29,4 +29,17 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+try 
+{
+    context.Database.Migrate();
+}
+catch (Exception ex)
+{
+    logger.LogError(ex, "A problem occurred during migration");
+}
+
 app.Run();
